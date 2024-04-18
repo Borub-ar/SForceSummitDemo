8b94f9f8d16940c1332eab33ff01a6462d48e261
Borub-ar <barabasz64@gmail.com>
2024-04-18 20:34:01 +0200

    Code refactoring

--- force-app/main/default/lwc/basicLightningModal/basicLightningModal.js
+++ force-app/main/default/lwc/basicLightningModal/basicLightningModal.js
@@ -6,4 +6,17 @@ export default class BasicLightningModal extends LightningModal {
     handleOkay() {
         this.close('okay');
     }
+
+    async handleOpen() {
+        const result = await MyModal.open({
+            // `label` is not included here in this example.
+            // it is set on lightning-modal-header instead
+            size: 'large',
+            description: 'Accessible description of modal\'s purpose',
+            content: 'Passed into content api',
+        });
+        // if modal closed with X button, promise returns result = 'undefined'
+        // if modal closed with OK button, promise returns result = 'okay'
+        console.log(result);
+    }
 }
--- force-app/main/default/lwc/rateMePage/rateMePage.js
+++ force-app/main/default/lwc/rateMePage/rateMePage.js
@@ -2,6 +2,8 @@ import { LightningElement } from 'lwc';
 import BACKGROUND from "@salesforce/resourceUrl/Background";
 import LOGO from "@salesforce/resourceUrl/Logo";
 
+import basicModal from 'c/basicLightningModal';
+
 export default class RateMePage extends LightningElement {
     background = BACKGROUND;
     craftwareLogo = LOGO
@@ -9,4 +11,9 @@ export default class RateMePage extends LightningElement {
     get backgroundStyle() {
         return `background-image: url(${this.background})`;
     }
+
+    async handleClick() {
+        this.result = await basicModal.open({size: 'small',description: 'Description'});
+    }   
+
 }
