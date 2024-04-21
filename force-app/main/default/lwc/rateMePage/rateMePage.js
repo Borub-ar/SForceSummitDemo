4567e7fae8d5068feecc8c3a7a447b1d0a81bc1f
Borub-ar <barabasz64@gmail.com>
2024-04-21 21:11:10 +0200

    Prettier Format

--- force-app/main/default/lwc/rateMePage/rateMePage.js
+++ force-app/main/default/lwc/rateMePage/rateMePage.js
@@ -1,26 +1,18 @@
 import { LightningElement } from 'lwc';
-import BACKGROUND from "@salesforce/resourceUrl/Background";
-import LOGO from "@salesforce/resourceUrl/Logo";
-
-// import basicModal from 'c/basicLightningModal';
-
+import BACKGROUND from '@salesforce/resourceUrl/Background';
+import LOGO from '@salesforce/resourceUrl/Logo';
 
 export default class RateMePage extends LightningElement {
-    showModal = false
+  showModal = false;
 
-    background = BACKGROUND;
-    craftwareLogo = LOGO
+  background = BACKGROUND;
+  craftwareLogo = LOGO;
 
-    get backgroundStyle() {
-        return `background-image: url(${this.background})`;
-    }
+  get backgroundStyle() {
+    return `background-image: url(${this.background})`;
+  }
 
-    // async handleClick() {
-    //     this.result = await basicModal.open({size: 'small',description: 'Description'});
-    // }   
-
-
-    handleMoodalVisibility() {
-        this.showModal = !this.showModal;
-    }
+  handleMoodalVisibility() {
+    this.showModal = !this.showModal;
+  }
 }
--- force-app/main/default/lwc/rateMeRating/rateMeRating.js
+++ force-app/main/default/lwc/rateMeRating/rateMeRating.js
@@ -1,40 +1,38 @@
-import {track, LightningElement} from 'lwc';
+import { track, LightningElement } from 'lwc';
 
 export default class RateMeRating extends LightningElement {
+  static delegatesFocus = true;
 
-    static delegatesFocus = true;
+  columns = 5;
 
-    columns = 5;
-
-    @track
-    options = [
-        {
-            value: "1",
-            label: "Nie wytrzymałem do końca ;)"
-        },
-        {
-            value: "2",
-            label: "Nic nowego się nie dowiedziałem"
-        },
-        {
-            value: "3",
-            label: "Było OK"
-        },
-        {
-            value: "4",
-            label: "Spoko, dużo ciekawych informacji"
-        },
-        {
-            value: "5",
-            label: "WOW, nie wiedziałem, że w LWC tak się da"
-        }
-    ]
-
-    handleValueChange(event) {
-        const value = event.target.value;
-        this.options.forEach(option => {
-            option.checked = option.value === value && event.target.checked === true;
-        });
+  @track
+  options = [
+    {
+      value: '1',
+      label: 'Nie wytrzymałem do końca ;)'
+    },
+    {
+      value: '2',
+      label: 'Nic nowego się nie dowiedziałem'
+    },
+    {
+      value: '3',
+      label: 'Było OK'
+    },
+    {
+      value: '4',
+      label: 'Spoko, dużo ciekawych informacji'
+    },
+    {
+      value: '5',
+      label: 'WOW, nie wiedziałem, że w LWC tak się da'
     }
+  ];
 
+  handleValueChange(event) {
+    const value = event.target.value;
+    this.options.forEach((option) => {
+      option.checked = option.value === value && event.target.checked === true;
+    });
+  }
 }
